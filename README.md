1cde9f267ffa2c89bfe8f5e12fbe5125bb72c99d
Luca <LucaT1@users.noreply.github.com>
2017-08-07 18:12:58 +0200

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+## Description
+VTerm-controls-windows is a core plugin for [`VTerm`](http://github.com/vterm/vterm) that adds window controls on Windows based machines
+
+***
+
+## Installation
+You can install this plugin by running `yarn` or `npm` in the VTerm's plugins folder as follows:
+```shell
+$ cd ~/.vterm/plugins
+$ yarn add vterm-controls-windows
+```
+OR
+```shell
+$ cd ~/.vterm/plugins
+$ npm i --save vterm-controls-windows
+```
---
+++ src/index.js
@@ -0,0 +1,40 @@
+import Store            from 'vterm/store'
+import { decorate }     from 'vterm/extend'
+
+import { remote }       from 'electron'
+import { h, Component } from 'preact'
+
+export default class Plugin {
+  // In the constructor we do several things:
+  //
+  constructor() {
+
+    @decorate('preTitleBar')
+    class WindowControls extends Component {
+
+      getStyles() {
+        const styles = {
+          float: 'right',
+          height: '100%'
+        }
+
+        return styles
+      }
+
+      render() {
+        const styles = this.getStyles()
+
+        // We don't need to import them 'cause babel
+        // bundles all the files inside the src folder
+        // and we can use the global variable!
+        return(
+          <div style={styles}>
+            <Close    />
+            <Maximize />
+            <Minimize />
+          </div>
+        )
+      }
+    }
+  }
+}
